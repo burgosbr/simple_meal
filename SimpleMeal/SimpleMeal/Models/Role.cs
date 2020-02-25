699db23e228646eb5fd27d7e4b8dc01e8918c6f0
Joelyson David <joelyson.davi@gmail.com>
2020-02-25 12:47:08 -0300

    Criação das classes

---
+++ SimpleMeal/SimpleMeal/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleMeal.Models
+{
+    public enum Role
+    {
+        Admin = 0,
+        Cook = 1,
+        Attendante = 2,
+        Client = 3
+    }
+}
